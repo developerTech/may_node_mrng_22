e3b46639b91883a36c6ec3af6ffb8a72d5416da0
aakashdeveloper <ahanda205@gmail.com>
2022-05-23 08:49:01 +0530

    xomapi

--- zomatoapi/apidoc.md
+++ zomatoapi/apidoc.md
@@ -12,20 +12,40 @@ http://localhost:9100/list/mealType
 # Restaurants WRT To MealType
 http://localhost:9100/restaurants?mealId=1
 # Restaurants WRT To Cuisine
+http://localhost:9100/filter/1?cuisineId=4
 # Restaurants WRT To Cost
+http://localhost:9100/filter/1?lcost=500&hcost=1000
 # Sort on basis of cost
+http://localhost:9100/filter/1?lcost=500&hcost=1000&sort=-1
 # Pagination
+http://localhost:9100/filter/1?lcost=500&hcost=2000&skip=4&limit=2
 
 //Page3
 # Details of Restaurants
+http://localhost:9100/details/11
 # Menu wrt to Restaurants
+http://localhost:9100/menu/8
 
 //Page4
 # Details of selected Menu
+(POST) http://localhost:9100/menuItem
+[4,5,23]
 # Place Order
+(POST) http://localhost:9100/placeOrder
+{
+	"id":767675687,
+	"userName":"Amit",
+	"phone":9879878997,
+	"rest_name":"Domino's Pizza",
+	"cost":789,
+	"status":"Payment Pending"
+}
+
 
 //Page5
 # Get All The Orders
+> http://localhost:9100/getOrder
 # GET Order on basis of emailId
+> http://localhost:9100/getOrder?email=a@a.com
 # Update the order status
 # Delete the orders
--- zomatoapi/app.js
+++ zomatoapi/app.js
@@ -4,7 +4,8 @@ let mongo = require('mongodb');
 let MongoClient = mongo.MongoClient;
 let dotenv = require('dotenv');
 dotenv.config();
-let mongoUrl = process.env.mongoLiveUrl;
+// let mongoUrl = process.env.mongoLiveUrl;
+let mongoUrl = process.env.mongoUrl;
 let bodyParser = require('body-Parser');
 let cors = require('cors');
 let port = process.env.PORT || 7100;
@@ -61,6 +62,100 @@ app.get('/restaurants',(req,res) => {
     })
 })
 
+//filters
+app.get('/filter/:mealId',(req,res) => {
+    let sort = {cost:1}
+    let skip = 0;
+    let limit = 1000000000;
+    let query ={}
+    let mealId = Number(req.params.mealId);
+    let cuisineId = Number(req.query.cuisineId);
+    let lcost = Number(req.query.lcost)
+    let hcost = Number(req.query.hcost)
+    if(req.query.sort){
+        sort={cost:req.query.sort}
+    }
+    if(req.query.skip && req.query.limit){
+        skip = Number(req.query.skip);
+        limit = Number(req.query.limit)
+    }
+    if(cuisineId&&lcost&&hcost){
+        query={
+            "mealTypes.mealtype_id":mealId,
+            "cuisines.cuisine_id":cuisineId,
+            $and:[{cost:{$gt:lcost,$lt:hcost}}]
+        }
+    }
+    else if(cuisineId){
+        query={
+                "mealTypes.mealtype_id":mealId,
+                "cuisines.cuisine_id":cuisineId
+            }
+    }else if(lcost&&hcost){
+        query={
+            "mealTypes.mealtype_id":mealId,
+            $and:[{cost:{$gt:lcost,$lt:hcost}}]
+        }
+    }
+
+    db.collection('restaurants').find(query).sort(sort).skip(skip).limit(limit).toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+/// restaurants details
+app.get('/details/:id',(req,res) => {
+    let id = Number(req.params.id)
+    //let id = mongo.ObjectId(req.params.id)
+    db.collection('restaurants').find({restaurant_id:id}).toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+/// menu wrt to restaurants
+app.get('/menu/:id',(req,res) => {
+    let restId = Number(req.params.id)
+    db.collection('menu').find({restaurant_id:restId}).toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
+
+/// menu wrt to ids
+app.post('/menuItem',(req,res) => {
+    console.log(req.body)
+    if(Array.isArray(req.body)){
+        db.collection('menu').find({menu_id:{$in:req.body}}).toArray((err,data) => {
+            if(err) throw err;
+            res.send(data)
+        })
+    }else{
+        res.send('Please Pass the Array Only')
+    }
+    
+})
+
+// place order
+app.post('/placeOrder',(req,res) => {
+    db.collection('orders').insert(req.body,(err,data) => {
+        if(err) throw err;
+        res.send('Order Placed')
+    })
+})
+
+//get Order
+app.get('/getOrder',(req,res) => {
+    let query = {}
+    if(req.query.email){
+        query = {email:req.query.email}
+    }
+    db.collection('orders').find(query).toArray((err,data) => {
+        if(err) throw err;
+        res.send(data)
+    })
+})
 
 
 app.get('/list/:item',(req,res) => {
@@ -73,7 +168,8 @@ app.get('/list/:item',(req,res) => {
 
 MongoClient.connect(mongoUrl,(err,client) => {
     if(err) console.log('Error While Connecting')
-    db = client.db('augintern');
+    //db = client.db('augintern');
+    db = client.db('internfeb');
     app.listen(port,() => {
         console.log(`Listing to port ${port}`)
     })
